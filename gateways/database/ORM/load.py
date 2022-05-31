b6e9a9afffff82e44eb107b44afdcfe0250ea751
Santiago Jaramillo <san_jara@outlook.com>
2022-05-30 19:34:58 -0500

    Creation LoadORM v1

--- gateways/database/ORM/load.py
+++ gateways/database/ORM/load.py
@@ -1,27 +1,42 @@
-from datetime import datetime
-from typing import Optional
+from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from gateways.database.ORM import Base
 
 
-class Load:
-    """Class that defines a single freight. Each freight represents Any shipment
-    over 150 lbs. Freight shipping is the transportation of goods, commodities
-    and cargo in bulk by ship, aircraft, truck or intermodal via train and road.
+class LoadORM(Base):
+    __tablename__ = "load"
+    # __table_args__ = {"schema": "flety"}
 
-    id: unique id of the Load
-
-    """
-
-    id: Optional[int]
-    pick_up_date: datetime
-    origin_city: str
-    origin_department: str
-    origin_address_id: Optional[int]
-    destiny_city: str
-    destiny_department: str
-    destiny_address_id: Optional[int]
-    weight: int
-    length: float
-    extra_info: str
-    contact_name: str
-    contact_phone: str
-    contact_email: str
+    id = Column(Integer, primary_key=True, index=True)
+    assigned_generator_id = Column(Integer, ForeignKey("generator.id"))
+    assigned_generator = relationship("GeneratorORM", backref="loads")
+    pick_up_date = Column(DateTime(timezone=True), nullable=False)
+    origin_city = Column(String(50), index=True, nullable=False)
+    origin_address_id = Column(Integer, ForeignKey("address.id"))
+    origin_address = relationship("AddressORM")
+    destiny_city = Column(String(50), index=True, nullable=False)
+    destiny_address_id = Column(Integer, ForeignKey("address.id"))
+    destiny_address = relationship("AddressORM")
+    contact_id = Column(Integer, ForeignKey("contact.id"))
+    contact = relationship("contactORM")
+    product_type = Column(String(50), nullable=False)
+    requirements = Column(String)
+    vehicle_type_id = Column(Integer, ForeignKey("vehicle_type.id"))
+    vehicle_type = relationship("VehicleTypeORM")
+    load_type_id = Column(Integer, ForeignKey("load_type.id"))
+    load_type = relationship("LoadTypeORM")
+    trailer_type_id = Column(Integer, ForeignKey("trailer_type.id"))
+    trailer_type = relationship("TrailerTypeORM")
+    weight_kg = Column(Float)
+    includes_loading = Column(Boolean, default=False, nullable=False)
+    loading_cost = Column(Float)
+    includes_unloading = Column(Boolean, default=False, nullable=False)
+    unloading_cost = Column(Float)
+    status = Column(String(20), default="pending", nullable=False)
+    assigned_vehicle_id = Column(Integer, ForeignKey("vehicle.id"))
+    assigned_vehicle = relationship("VehicleORM", backref="loads")
+    assigned_company_id = Column(Integer, ForeignKey("transport_company.id"))
+    assigned_company = relationship("TransportCompanyORM", backref="loads")
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    updated_at = Column(DateTime(timezone=True))
